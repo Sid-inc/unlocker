198478a157ebc3283072fef876e1744b7a760e35
ionovnn <ionovnn@monqlab.com>
2023-08-01 08:46:14 +0300

    add mobile menu

--- app/js/app.min.js
+++ app/js/app.min.js
@@ -1,6 +1,9 @@
 const header = document.querySelector('.main-header');
 const scrollChange = 16;
 
+const nav = document.querySelector('.main-nav');
+const navTrigger = document.querySelector('.main-header__menu-trigger');
+
 let scrollpos = window.scrollY;
 
 function fixHeaderOnTop() {
@@ -11,8 +14,25 @@ function freeHeaderOnTop() {
   header.classList.remove("main-header--fixed");
 }
 
+function changeMenuState() {
+  navTrigger.classList.toggle('main-header__menu-trigger--opened');
+  nav.classList.toggle('main-nav--opened');
+}
+
 window.addEventListener('scroll', function() {
   scrollpos = this.window.scrollY;
   if (scrollpos >= scrollChange) fixHeaderOnTop()
   else freeHeaderOnTop();
+});
+
+navTrigger.addEventListener('click', () => {
+  changeMenuState();
+});
+
+nav.addEventListener('click', (event) => {
+  const target = event.target;
+  
+  if (target.classList.contains('main-nav__link')) {
+    changeMenuState();
+  }
 });
